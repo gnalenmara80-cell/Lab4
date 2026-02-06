cebd9bbaf2151c521567ba6a53a2ef3eb253bbde
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-06 16:55:15 -0500

    Add Lab4_gnalen-1.py completed card dealing program

---
+++ Lab4_gnalen-1.py
@@ -0,0 +1,13 @@
+""" 
+Program Name: Lab4_Deal Cards
+Author: Gnalen Mara
+Purpose: This program simulates dealing a hand of cards. The user chooses how many
+cards they want, and the program randomly generates that many unique cards from
+standard card values and suits. Cards do not repeat in the hand.
+Starter Code: No starter code used.
+Date: 02/06/2026
+"""
+
+import random
+# list of card values and suits
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',] 
